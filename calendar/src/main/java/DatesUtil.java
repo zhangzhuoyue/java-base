50fb7b0c2eba6f7166a28d7dc9a19375424b8240
zhanghzuoyue <szy6481@163.com>
2020-06-18 18:09:13 +0800

    DATE_OF_YEAR  DATE_OF_MONTH

--- calendar/src/main/java/DatesUtil.java
+++ calendar/src/main/java/DatesUtil.java
@@ -1,18 +1,27 @@
 import org.junit.Test;
 
-import java.sql.Timestamp;
-import java.text.ParseException;
+
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
-import java.util.Random;
 
+/*
+* cal1.add(Calendar.DAY_OF_MONTH,1);
+cal1.add(Calendar.DAY_OF_YEAR,1);
+cal1.add(Calendar.DATE,1);
+* 就单纯的add操作结果都一样，因为都是将日期+1
+* 但是Calendar设置DAY_OF_MONTH和DAY_OF_YEAR的目的不是用来+1
+* AY_OF_MONTH的主要作用是cal.get(DAY_OF_MONTH)，用来获得这一天在是这个月的第多少天
+Calendar.DAY_OF_YEAR的主要作用是cal.get(DAY_OF_YEAR)，用来获得这一天在是这个年的第多少天。
+同样，还有DAY_OF_WEEK，用来获得当前日期是一周的第几天
+*
+* */
 public class DatesUtil {
 
     @Test
     public void test(){
-        Date dayBegin = getBeginDayOfYesterday();
+        Date dayBegin =  getEndDayOfYesterday();
         //Date dayEnd = getDayEnd();
         SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
         String format1 = format.format(dayBegin);
@@ -20,7 +29,7 @@ public class DatesUtil {
     }
 
 
-    //获取当天开始时间  例如：2020-06-18 00:00:00
+    /*获取当天开始时间  例如：2020-06-18 00:00:00*/
     public static Date getDayBegin(){
         Calendar cal = Calendar.getInstance();
         cal.set(Calendar.HOUR_OF_DAY,0);//Calendar.HOUR_OF_DAY是24小时制;Calendar.HOUR是12小时制
@@ -49,6 +58,30 @@ public class DatesUtil {
         Calendar cal = new GregorianCalendar();
         cal.setTime(getDayBegin());
         cal.add(Calendar.DAY_OF_MONTH,-1);//add 对日期增减
+        return cal.getTime();
+    }
+
+    //获取昨天结束时间
+    public static Date getEndDayOfYesterday(){
+        GregorianCalendar cal = new GregorianCalendar();
+        cal.setTime(getDayEnd());
+        cal.add(Calendar.DAY_OF_MONTH,-1);
+        return cal.getTime();
+    }
+
+   /* 获取明天的开始时间*/
+    public static Date getBeginDayOfTomorrow(){
+        Calendar cal = new GregorianCalendar();
+        cal.setTime(getDayBegin());
+        cal.add(Calendar.DAY_OF_MONTH,1);
+        return cal.getTime();
+    }
+
+    /*获取明天的结束时间*/
+    public static Date getEndDayOfTomorrow(){
+        Calendar cal = new GregorianCalendar();
+        cal.setTime(getDayEnd());
+        cal.add(Calendar.DAY_OF_MONTH,1);
         return cal.getTime();
     }
 
