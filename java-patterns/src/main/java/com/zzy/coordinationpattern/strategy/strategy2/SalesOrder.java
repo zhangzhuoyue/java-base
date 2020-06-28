d61f6e4d4af774e9360751bab141012a3e8778ee
zy <szy6481@163.com>
2020-06-28 16:52:05 +0800

    策略模式

---
+++ java-patterns/src/main/java/com/zzy/coordinationpattern/strategy/strategy2/SalesOrder.java
@@ -0,0 +1,29 @@
+package com.zzy.coordinationpattern.strategy.strategy2;
+
+/**
+ * @author zzy
+ * @date 2020/6/28 16:28
+ */
+
+/**
+ * strategy客户端在全过程中是稳定的，对于算法子类变化。由抽象类隔离
+ */
+public class SalesOrder {
+
+    private TaxStrategy taxStrategy;
+
+    /**
+     * 这里传递工厂对象，有工厂决定传递哪个TaxStrategy,使用到工厂模式
+     * @param taxStrategyFactory
+     */
+    public SalesOrder(TaxStrategyFactory taxStrategyFactory){
+        this.taxStrategy = taxStrategyFactory.newStrategy();
+    }
+
+    public Double calaulate(Object object){
+        //...
+        //获取上限文信息
+        taxStrategy.calculate(object);//多态调用，依赖于工厂返回哪个子类,减少了无用代码的加载，使得CPU缓存中可以加载更多有效执行代码片段
+        return null;
+    }
+}
---
+++ java-patterns/src/main/java/com/zzy/coordinationpattern/strategy/strategy2/USTax.java
@@ -0,0 +1,12 @@
+package com.zzy.coordinationpattern.strategy.strategy2;
+
+/**
+ * @author zzy
+ * @date 2020/6/28 16:26
+ */
+public class USTax extends TaxStrategy {
+    @Override
+    protected Double calculate(Object object) {
+        return null;
+    }
+}
