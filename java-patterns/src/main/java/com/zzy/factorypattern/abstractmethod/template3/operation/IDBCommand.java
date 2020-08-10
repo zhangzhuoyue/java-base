e620b517635b97e8e4a8c9ea0c90bf2bbbb4d5a5
zy <szy6481@163.com>
2020-08-10 21:45:29 +0800

    abstract Factory finally

---
+++ java-patterns/src/main/java/com/zzy/factorypattern/abstractmethod/template3/operation/IDBCommand.java
@@ -0,0 +1,9 @@
+package com.zzy.factorypattern.abstractmethod.template3.operation;
+
+/**
+ * @author zhangyue666
+ * @description 数据库命令执行基类
+ * @date 2020/8/4 22:03
+ */
+public abstract class IDBCommand {
+}
---
+++ java-patterns/src/main/java/com/zzy/factorypattern/abstractmethod/template3/operationimpl/mysql/MysqlCommand.java
@@ -0,0 +1,12 @@
+package com.zzy.factorypattern.abstractmethod.template3.operationimpl.mysql;
+
+import com.zzy.factorypattern.abstractmethod.template3.operation.IDBCommand;
+
+/**
+ * @author zhangyue666
+ * @description
+ * @date 2020/8/4 22:19
+ */
+public class MysqlCommand extends IDBCommand {
+
+}
