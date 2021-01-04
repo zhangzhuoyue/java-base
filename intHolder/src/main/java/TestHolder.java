8e494807d15b285bbcf41bbb9a84c86572089ae2
zhangzhuoyue <syz6481@163.com>
2021-01-04 13:55:24 +0800

    持有者类型，基本类型值传递

---
+++ intHolder/src/main/java/TestHolder.java
@@ -0,0 +1,22 @@
+import org.omg.CORBA.IntHolder;
+
+/**
+ * @author zzy
+ * @data 2021/1/4
+ * Integer    无法将修改数据传递，因为value属性是 final ，每次修改创建一个新的值
+ * IntHolder  可以将修改数据传递，因为value属性非 final ，每次修改共用一个地址
+ */
+public class TestHolder {
+
+    public static void main(String[] args) {
+        IntHolder intHolder = new IntHolder(12);
+        test1(intHolder);
+
+    }
+
+
+    public static void test1(IntHolder holder) {
+        holder.value = 2 + holder.value;
+    }
+
+}
