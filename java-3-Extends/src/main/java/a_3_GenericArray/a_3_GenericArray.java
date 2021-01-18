f9f0dc4fc24b01de8c66bd791addcb8a3569203e
zy <szy6481@163.com>
2021-01-18 22:15:15 +0800

    java 泛型数组-》ArrayList

---
+++ java-3-Extends/src/main/java/a_3_GenericArray/a_3_GenericArray.java
@@ -0,0 +1,21 @@
+package a_3_GenericArray;
+
+import java.util.ArrayList;
+
+/**
+ * @author ze666
+ * @desc
+ * @date 2021/1/18 20:54
+ */
+public class a_3_GenericArray {
+
+    public static void main(String[] args) {
+        /**
+         * ArrayList 动态数组，
+         * 1. 截取多余存储空间。trimeSize()在确定不再变化时使用。
+         */
+        ArrayList<String> arrayList = new ArrayList<>();
+        arrayList.toArray();//将List转换成数组/将集合转换到指定的集合中
+    }
+
+}
