bb5dd0817f61602d299e12516a20036e863d5f8e
zy <szy6481@163.com>
2021-01-18 22:14:33 +0800

    java 自动装箱

---
+++ java-3-Extends/src/main/java/a_4_AutoBox/a_4_AutoBox.java
@@ -0,0 +1,16 @@
+package a_4_AutoBox;
+
+/**
+ * @author ze666
+ * @desc
+ * @date 2021/1/18 21:51
+ */
+public class a_4_AutoBox {
+
+    public static void main(String[] args) {
+        int num = 123;
+        String s = Integer.toBinaryString(num);//生成二进制
+
+    }
+
+}
