3ee8af643b84708c7ffe2cbf5eef114250a8dbce
zy <szy6481@163.com>
2021-01-18 22:28:54 +0800

    测试

---
+++ java-3-Extends/src/test/java/Test.java
@@ -0,0 +1,14 @@
+import a_1_ExtendsRelation.Son;
+
+/**
+ * @author ze666
+ * @desc
+ * @date 2021/1/18 19:24
+ */
+public class Test {
+    public static void main(String[] args) {
+        Son son = new Son();
+        String name = son.getName();
+        String getjj = son.getjj();
+    }
+}
